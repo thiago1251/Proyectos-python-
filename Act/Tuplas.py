0387e9e163e72c0cb7b4e6939e5a801d7a983e77
thiago1251 <115585014+thiago1251@users.noreply.github.com>
2022-11-14 10:30:59 -0500

    Update Tuplas.py

--- Act/Tuplas.py
+++ Act/Tuplas.py
@@ -1,35 +1,35 @@
 t = (23,'a',(2.5,3.7,'x'),["perrito","gatito"],'Pepe')
-print (t) #imprime el arreglo
-print (len(t)) #Cuenta la cantidad de objetos que hay en la tupla
+print (t) #imprime la tupla creada
+print (len(t)) #Cuenta la cantidad de elementos que existen en la tupla
 
 
 print ('=====================================')
-print (t[0]) #Imprime el primer valor de la tupla
+print (t[0]) #Imprime el valor de la posicion especificada de la tupla
 print (t[3]) #Imprime el cuarto valor de la tupla
-print (t[1:3]) #Imprime el segundo y cuarto valor de la tupla
-print (t[3][1]) #Imprime el segundo valor que hay dentro del cuarto objeto de la tupla
+print (t[1:3]) #Imprime el segundo y cuarto elemento de la tupla
+print (t[3][1]) #Imprime el segundo elemnto que hay dentro del cuarto objeto de la tupla
 
 
 print ('====================================')
 print (t[3]) #Imprime el tercer objeto de la tupla
-t[3].append('lorito') #agrega un valor al tercer objeto de la tupla, en este caso 'lorito'
-print (t) #Imprime la tupla modificada
+t[3].append('lorito') #agrega un valor al tercer objeto de la tupla
+print (t) #Imprime la tupla con los cambios
 
 print ('====================================')
 for elemento in t:
-    print (elemento) #Imprime cada uno de los valores de la tupla en lineas separadas
+    print (elemento) #Imprime cada uno de los elementos de la tupla en lineas separadas
 
 print ('====================================')
 for index in range(0,len(t)):
-    print (t[index])  #Imprime los valores hasta el valor específicado
+    print (t[index])  #Imprime los valores hasta el valor del rago especificado
 
 print ('====================================')
 if 'a' in t:
-    print ("El elemento 'a' esta en la tupla") #Si hay un valor 'a' en el arreglo, se imprime la línea
+    print ("El elemento 'a' esta en la tupla") #Si hay un elemento 'a' en la tupla, se imprimira la línea
 
 print ('====================================')
-lista=list(t) #transforma la tupla en una lista
-lista[1]='A'  #reemplaza segundo valor de la lista por una A
+lista=list(t) #cambia la tupla en una lista
+lista[1]='A'  #reemplaza el segundo elemento de la lista por una A
 print (lista) #se imprime la lista
 
 tupla=tuple(lista) #cambia la lista a una tupla
